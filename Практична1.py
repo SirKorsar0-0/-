2ba88206fecc799d095598bf407fa7580896be46
SirKorsar0-0 <byradenis37@gmail.com>
2024-09-16 13:36:06 +0300

    Add files via upload

---
+++ Практична1.py
@@ -0,0 +1,10 @@
+world = "Hellow world"
+first_name = "Денис"
+last_name="Бура"
+age = 16
+
+print("Тип данний змінної світ : ",type(world))
+print("Тип данний змінної ім'я : ",type(first_name))
+print("Тип данний змінної вік : ",type(age))
+print("Тип данний змінної призвіщем : ",type(last_name))
+
