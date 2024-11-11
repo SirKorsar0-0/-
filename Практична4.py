e8854abaf238b61cbc650bd178787765e37ee562
SirKorsar0-0 <byradenis37@gmail.com>
2024-11-11 14:02:20 +0200

    Update Практична4.py

--- Практична4.py
+++ Практична4.py
@@ -1,13 +1,16 @@
-def sort(list):
+def remove_duplicates(input_list):
     element = list(set(input_list))
+    return element
 
-    int_sort = sorted([x for x in element if isinstance(x, (int, float))])
-    str_sort = [x for x in element if not isinstance(x, (int, float))]
-
+def sort_int_str(input_list):
+    int_sort = sorted([x for x in input_list if isinstance(x, (int, float))])
+    str_sort = sorted([x for x in input_list if isinstance(x, str)], key=str.lower)
     return int_sort + str_sort
 
+input_list = [1, 2, 3, 4, 5, 6, 3, 4, 5, 7, 6, 5, 4, 3, 4, 5, 4, 3, 'Привіт', 'анаконда']
 
-list = [1,2,3,4,5,6,3,4,5,7,6,5,4,3,4,5,4,3, 'Привіт', 'анаконда']
-result = sort(list)
+result = remove_duplicates(input_list)
+
+result = sort_int_str(result)
 
 print(result)
